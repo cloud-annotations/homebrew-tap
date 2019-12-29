a41dac8ca95a6c1ac380a2f3928b95e67e3c8609
Nick Bourdakos <bourdakos1@gmail.com>
2019-12-28 21:42:42 -0500

    Update cacli.rb

--- cacli.rb
+++ cacli.rb
@@ -1,2 +1,25 @@
 class Cacli < Formula
-  desc "CLI for trainin
+  desc "CLI for training Cloud Annotation object detection and classification models"
+  homepage "https://cloud.annotations.ai"
+  url "https://github.com/cloud-annotations/training/archive/v1.22.333.tar.gz"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["XC_OS"] = "darwin"
+    ENV["XC_ARCH"] = "amd64"
+    ENV["GOPATH"] = buildpath
+    (buildpath/"src/github.com/cloud-annotations/training").install buildpath.children
+    cd "src/github.com/cloud-annotations/training/cacli" do
+      project = "github.com/cloud-annotations/training/cacli"
+      system "go", "build",
+             "-ldflags", "-s -w -X #{project}/version.Version=#{version}",
+             "-o", bin/"cacli"
+      prefix.install_metafiles
+    end
+  end
+
+  test do
+    system "#{bin}/cacli", "--version"
+  end
+end
