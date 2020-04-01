e0cfff8d505ddeb978a22b79801fde7456bbff13
Nick Bourdakos <bourdakos1@gmail.com>
2020-04-01 16:29:02 -0400

    Update cacli.rb

--- cacli.rb
+++ cacli.rb
@@ -1,7 +1,7 @@
 class Cacli < Formula
   desc "Train machine learning models from Cloud Annotations"
   homepage "https://cloud.annotations.ai"
-  url "https://github.com/cloud-annotations/training/archive/v1.3.0.gz"
+  url "https://github.com/cloud-annotations/training/archive/v1.3.0.tar.gz"
 
   depends_on "go" => :build
 
